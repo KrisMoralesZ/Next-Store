838ac48ca7d806ffa2147bd444b2b90c051b9df2
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-10-16 09:31:16 -0700

    Create Jest Configuration

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+/** @type {import('jest').Config} */
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|scss|sass)$': 'identity-obj-proxy',
+  },
+};
+
+module.exports = createJestConfig(customJestConfig);
