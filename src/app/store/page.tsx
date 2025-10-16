58f4f4aafa41330cb10422d3c5efa099e32819f3
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-10-15 19:38:35 -0700

    Fetch Data From API

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -1,6 +1,22 @@
 import styles from "./page.sass";
 
-const StorePage = () => {
+const getProducts = async () => {
+  const response = await fetch(
+    `${process.env.SHOPIFY_HOSTNAME}/admin/api/2023-10/products.json`,
+    {
+      headers: new Headers({
+        "X-Shopify-Access-Token": process.env.SHOPIFY_ACCESS_TOKEN || "",
+      }),
+    }
+  );
+  const data = await response.json();
+  return data;
+};
+
+const StorePage = async () => {
+  const products = await getProducts();
+  console.log("Store Page Rendered", products);
+
   return (
     <div className={styles.storePage}>
       <h1 className={styles.title}>Store Page</h1>
