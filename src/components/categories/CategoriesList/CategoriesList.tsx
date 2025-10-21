ca1ed2f6da6209699ab7468c23c7833e15ac35b5
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-10-21 14:01:55 -0700

    Update Header to Show Dynamic Categories

---
+++ src/components/categories/CategoriesList/CategoriesList.tsx
@@ -0,0 +1,29 @@
+import { getCollections } from "@/services/Collections/request";
+import Link from "next/link";
+import styles from "./CategoriesList.module.sass";
+
+const CategoriesList = async () => {
+  const categories = await getCollections();
+  console.log(categories);
+
+  return (
+    <div className={styles.CategoriesList__list}>
+      {categories ? (
+        categories.smart_collections.map(
+          (collection: { id: string; title: string }) => (
+            <Link
+              href={`/categories/${collection.title}`}
+              key={collection.id}
+              className={styles.CategoriesList__link}
+            >
+              {collection.title}
+            </Link>
+          )
+        )
+      ) : (
+        <li>No categories found</li>
+      )}
+    </div>
+  );
+};
+export default CategoriesList;
