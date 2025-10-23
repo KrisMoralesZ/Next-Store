8a321b2b4ec51475adb59823ea03d7b10bd3a117
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-10-23 11:13:33 -0700

    Update Card to FC Component

--- src/components/shared/Card/Card.tsx
+++ src/components/shared/Card/Card.tsx
@@ -1,15 +1,57 @@
+// src/components/shared/Card.tsx
+import React from "react";
+import styles from "./Card.module.sass";
+
 export interface CardProps {
   title?: string;
   image?: string;
   description?: string;
+  width?: number;
+  height?: number;
 }
 
-const Card = ({ title, image, description }: CardProps) => {
+const Card: React.FC<CardProps> = ({
+  title,
+  image,
+  description,
+  width = 400,
+  height = 250,
+}) => {
   return (
-    <div>
-      <h2>{title}</h2>
-      <img src={image} height={200} width={200} alt="Card Image" />
-      <p>{description}</p>
+    <div className={styles.Card__Container}>
+      <h2 className={styles.Card__Header}>{title}</h2>
+      <div className={styles.Card__Content}>
+        {image ? (
+          // eslint-disable-next-line @next/next/no-img-element
+          <img
+            src={image}
+            alt={title ?? "Card image"}
+            width={width}
+            height={height}
+            style={{
+              objectFit: "cover",
+              display: "block",
+              width: "100%",
+              height,
+            }}
+          />
+        ) : (
+          <div
+            style={{
+              width: "100%",
+              height,
+              background: "#eee",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              color: "#666",
+            }}
+          >
+            No image
+          </div>
+        )}
+        <p>{description}</p>
+      </div>
     </div>
   );
 };
