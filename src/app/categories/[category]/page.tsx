e6f40ba42955eff5aa60fc2c6ddfe0af26444474
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-10-23 17:30:53 -0700

    CollectionProducts Request

--- src/app/categories/[category]/page.tsx
+++ src/app/categories/[category]/page.tsx
@@ -1,5 +1,8 @@
 import { CategoryCards } from "@/components/categories/CategoryCards";
-import { getCollectionById } from "@/services/Collections/SingleCollection/request";
+import {
+  getCollectionProducts,
+  getSingleCollectionById,
+} from "@/services/Collections/request";
 
 export default async function CategoryPage({
   params,
@@ -7,12 +10,14 @@ export default async function CategoryPage({
   params: { category: string };
 }) {
   const id = params.category;
-  const data = await getCollectionById(id);
+  const data = await getSingleCollectionById(id);
+  const collectionProducts = await getCollectionProducts(id);
 
   return (
     <div>
       <h1>Collection ID: {id}</h1>
       <pre>{JSON.stringify(data, null, 2)}</pre>
+      <pre>{JSON.stringify(collectionProducts, null, 2)}</pre>
       <CategoryCards />
     </div>
   );
