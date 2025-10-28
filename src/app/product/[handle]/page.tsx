d7f2d9864efff4235d70db907db81e883ccf7288
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-10-28 13:39:07 -0700

    Update Page Component

--- src/app/product/[handle]/page.tsx
+++ src/app/product/[handle]/page.tsx
@@ -1,5 +1,18 @@
-const ProductPage = () => {
-  return <div>Product Page</div>;
+import { ProductView } from "@/components/products/ProductView";
+import { getProducts } from "@/services/Products/request";
+
+interface ProductPageProps {
+  searchParams: {
+    id: string;
+  };
+}
+
+const ProductPage = async ({ searchParams }: ProductPageProps) => {
+  const id = searchParams.id;
+  const products = await getProducts(id);
+  const product = products[0];
+
+  return <ProductView product={product} />;
 };
 
 export default ProductPage;
