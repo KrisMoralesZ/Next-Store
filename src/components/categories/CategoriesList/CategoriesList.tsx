28ec50ebe809e858610dacc304eaba66e53efbd1
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-10-28 19:16:11 -0700

    Refactor CategoriesList

--- src/components/categories/CategoriesList/CategoriesList.tsx
+++ src/components/categories/CategoriesList/CategoriesList.tsx
@@ -1,28 +1,53 @@
+"use client";
+import { useEffect, useState } from "react";
 import { getCollections } from "@/services/Collections/request";
 import Link from "next/link";
 import styles from "./CategoriesList.module.sass";
 
-const CategoriesList = async () => {
-  const categories = await getCollections();
+interface Collection {
+  id: string;
+  title: string;
+}
 
-  const list = categories?.smart_collections ?? [];
+const CategoriesList = () => {
+  const [categories, setCategories] = useState<Collection[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchCollections = async () => {
+      try {
+        const data = await getCollections();
+        setCategories(data?.smart_collections || []);
+        console.log("@@@@@data", data);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchCollections();
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
 
   return (
     <div className={styles.CategoriesList__list}>
-      {list.length > 0 ? (
-        list.map((collection: { id: string; title: string }) => (
+      {categories.length > 0 ? (
+        categories.map((c) => (
           <Link
-            href={`/categories/${collection.title}`}
-            key={collection.id}
+            key={c.id}
+            href={`/categories/${c.title}`}
             className={styles.CategoriesList__link}
           >
-            {collection.title}
+            {c.title}
           </Link>
         ))
       ) : (
-        <li>No categories found</li>
+        <p>No categories found</p>
       )}
     </div>
   );
 };
+
 export default CategoriesList;
